378c29b2562253390f2465e07c2c1c96ede37935
Leon Nitsch <leonnitsch170@gmail.com>
2023-03-26 21:51:33 -0400

    Commit 0: Creation of Makefile and basic structure

---
+++ Mergesort/libParseArgs.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libParseArgs.h"
+
+//Function to parse arguements from the command line
---
+++ Mergesort/mergeSort.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libSort.h"
+#include "libParseArgs.h"
+
+//Function to print out the usage of the program
+void usage(FILE *fp){}
+
+//Main function to run the program
+int main(int argc, char *argv[]){}
